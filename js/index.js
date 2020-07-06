76de5e0bc03f15f4a1bb20147de6777d02bf1824
prycedom <prycedom@gmail.com>
2020-07-05 21:15:31 -0400

    Finished exceeds stuff and touchups

--- js/index.js
+++ js/index.js
@@ -1,6 +1,66 @@
 const notificationIcon = document.getElementById("notifications-icon");
 const notifications = document.getElementById("notifications");
+const sendEmailNotifications = document.getElementById("send-email-notifications");
+const profileVisibility = document.getElementById("profile-visibility");
+const timezone = document.getElementById("timezone");
+const sendMessage = document.getElementById("message-send");
+const searchUser = document.getElementById("searchUser");
+const messageUser = document.getElementById("messageUser");
+const searchUserError = document.getElementById("searchUser-error");
+const messageUserError = document.getElementById("messageUser-error");
+const messageSendConfirmation = document.getElementById("message-send-confirmation");
 
 notificationIcon.addEventListener("click", () => {
     notifications.classList.toggle("notification-pop-up-hidden");
 });
+
+document.getElementById("settings-save").addEventListener("click", () => {
+    localStorage.setItem("profileVisibility", profileVisibility.checked);
+    localStorage.setItem("sendEmailNotifications", sendEmailNotifications.checked);
+    localStorage.setItem("timezone", timezone.value);
+});
+
+document.getElementById("settings-cancel").addEventListener("click", () => {
+    loadSettings();
+});
+
+searchUser.addEventListener("keyup", () => {
+    searchUserError.innerHTML = "";
+})
+
+messageUser.addEventListener("keyup", () => {
+    messageUserError.innerHTML = "";
+})
+
+sendMessage.addEventListener("click", (e) => {
+    e.preventDefault();
+    let hasErrors = false;
+    searchUserError.innerHTML = "";
+    messageUserError.innerHTML = "";
+    if(!searchUser.value) {
+        searchUserError.innerHTML = "This has to have a value.";
+        hasErrors = true;
+    }
+    
+    if(!messageUser.value) {
+        messageUserError.innerHTML = "This has to have a value.";
+        hasErrors = true;
+    }
+
+    if(hasErrors == false) {
+        messageSendConfirmation.innerHTML = "Message was sent to " + searchUser.value + "!";
+        searchUser.value = "";
+        messageUser.value = "";
+        setTimeout(() => {
+            messageSendConfirmation.innerHTML = "";
+        }, 5000);
+    }
+});
+
+function loadSettings() {
+    profileVisibility.checked = (localStorage.getItem("profileVisibility") == "true");
+    sendEmailNotifications.checked = (localStorage.getItem("sendEmailNotifications") == "true");
+    timezone.value = localStorage.getItem("timezone");
+}
+
+loadSettings();
